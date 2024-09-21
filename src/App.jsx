bb126243a98539f9bea59f41ea65127e604feb1a
abhay gupta <abhaykrgupta098@gmail.com>
2024-09-21 14:24:31 +0530

    Initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import FeatureSection from "./components/FeatureSection";
+import Footer from "./components/Footer";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import Price from "./components/Price";
+import Testimonial from "./components/Testimonial";
+import Workflow from "./components/Workflow";
 
 export default function App() {
   return (
@@ -7,6 +12,11 @@ export default function App() {
       <Navbar />
       <div className="max-w-7xl mx-auto pt-20 px-6">
         <HeroSection />
+        <FeatureSection/>
+        <Workflow/>
+        <Price/>
+        <Testimonial/>
+        <Footer/>
       </div>
     </>
   );
